afc07d43a19374339b1bdca799b6a73c10f49def
Claudia Cadec <claudiacadec@outlook.com>
2023-04-22 21:23:05 +0200

    commit

--- lib/screens/counter/counter_screen.dart
+++ lib/screens/counter/counter_screen.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 
-class CounterScreen extends StatelessWidget {
-  const CounterScreen({super.key});
+class CounterFuntionsScreen extends StatefulWidget {
+  const CounterFuntionsScreen({super.key});
 
+  @override
+  State<CounterFuntionsScreen> createState() => _CounterFuntionsScreenState();
+}
+  int clickCounter = 0;
+class _CounterFuntionsScreenState extends State<CounterFuntionsScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,22 +20,55 @@ class CounterScreen extends StatelessWidget {
         style: TextStyle(color: Color.fromARGB(255, 143, 234, 146)),
         ),
       ),
-       body: Center(
-        child: Column(
-          
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: const [
-            Text("10", style: TextStyle(fontSize: 160, fontWeight:FontWeight.w100,color: Color.fromARGB(255, 6, 70, 8) ),),
-            Text("Clicks",style: TextStyle(fontSize: 89, fontWeight:FontWeight.w100, color: Color.fromARGB(255, 6, 70, 8)),),
+       body: Padding(
+         padding: const EdgeInsets.all(100.0),
+         child: Center(
+          child: Column(
+            
+            mainAxisAlignment: MainAxisAlignment.start,
+            children:  [
+              Text( "$clickCounter" , style: const TextStyle(fontSize: 150, fontWeight:FontWeight.w100,color: Color.fromARGB(255, 6, 70, 8) ),),
+              Text("Click${clickCounter == 1 ? "" : "s"}",style: const TextStyle(fontSize: 65, fontWeight:FontWeight.w300, color: Color.fromARGB(255, 6, 70, 8)),),
+            ],
+          )
+          ),
+       ),
+        floatingActionButton: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            FloatingActionButton(
+              backgroundColor: const Color.fromARGB(255, 6, 70, 8),
+              onPressed:(){
+                setState(() {
+                  clickCounter++;
+                });
+              },
+              child: const Icon(Icons.plus_one,color: Color.fromARGB(255, 143, 234, 146),
+              ),),
+              SizedBox(height: 10,),
+
+              FloatingActionButton(
+              backgroundColor: const Color.fromARGB(255, 6, 70, 8),
+              onPressed:(){
+                setState(() {
+                  clickCounter--;
+                });
+              },
+              child: const Icon(Icons.exposure_minus_1_outlined,color: Color.fromARGB(255, 143, 234, 146),
+              ),),
+              SizedBox(height: 10,),
+
+              FloatingActionButton(
+              backgroundColor: const Color.fromARGB(255, 6, 70, 8),
+              onPressed:(){
+                setState(() {
+                  clickCounter=0;
+                });
+              },
+              child: const Icon(Icons.refresh_outlined,color: Color.fromARGB(255, 143, 234, 146),
+              ),),
           ],
         )
-        ),
-        floatingActionButton: FloatingActionButton(
-          backgroundColor: Color.fromARGB(255, 6, 70, 8),
-          onPressed:(){},
-          child: const Icon(Icons.plus_one,color: Color.fromARGB(255, 143, 234, 146),
-          
-           ),)
       );
   }
 }
